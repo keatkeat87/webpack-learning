c339e729143b5efdf38c6839479cd4a319d938c4
Derrick Yam <keatkeat87@live.com>
2021-05-01 11:53:06 +0800

    ok

--- src/about/index.ts
+++ src/about/index.ts
@@ -1,7 +1,7 @@
 import {
-    value
-    // html
+    value,
+    html
 } from '../shared/index';
 
 console.log('contact page', value);
-// html('#target', 'about page');
+html('#target', 'about page');
