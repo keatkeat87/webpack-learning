f21efaf182f445fe5128fe9078fc213fe9eb841e
Derrick Yam <keatkeat87@live.com>
2021-05-02 11:45:21 +0800

    ok

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    extends: ['stylelint-config-recommended'],
+    extends: [
+        // 'stylelint-config-recommended'
+    ],
     rules: {
         'at-rule-no-unknown': [
             true,
