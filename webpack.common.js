aa655cf56f208ef985214934916d2a75fe856325
Mark Koo <koocool88@gmail.com>
2021-05-03 18:29:43 +0800

    ok

--- webpack.common.js
+++ webpack.common.js
@@ -97,7 +97,8 @@ module.exports = {
                 commons: {
                     name: 'commons',
                     chunks: 'all',
-                    minChunks: 2
+                    minChunks: 2,
+                    minSize: 1
                 },
                 vendors: {
                     name: 'vendors',
