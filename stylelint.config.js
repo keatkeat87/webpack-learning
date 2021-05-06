c37f5bbd810d07d32d9c7908dff59fc837762e88
Mark Koo <koocool88@gmail.com>
2021-05-06 13:44:04 +0800

    ok

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,12 @@ module.exports = {
     extends: [
         // 'stylelint-config-recommended'
     ],
+    plugins: [
+        'stylelint-scss'
+    ],
     rules: {
-        'at-rule-no-unknown': [
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': [
             true,
             {
                 ignoreAtRules: [
